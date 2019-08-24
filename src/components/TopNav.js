97343423131e520c23121dbdec20a2e857498189
Brandon <brandongines@gmail.com>
2019-08-24 16:09:18 -0400

    layout test

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,23 +1,7 @@
 import React, { Component } from "react"
-import MoblieNav from "./MoblieNav"
 
 export default class TopNav extends Component {
   render() {
-    return (
-      <div>
-        {" "}
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item active">
-                  <li>{/* <MoblieNav /> */}</li>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
-    )
+    return <div></div>
   }
 }
