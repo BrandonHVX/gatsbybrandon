6e05713168b56c6f349a481bf6478d2087398ef7
Brandon <brandongines@gmail.com>
2019-08-27 17:03:04 -0400

    layout

--- src/components/ProjectsTabs.js
+++ src/components/ProjectsTabs.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap"
 import "react-tabs/style/react-tabs.css"
 import WebDev from "./WebDev"
 import Videos from "./Videos"
+import AboutMe from "./AboutMe"
 
 const tabs = {
   backgroundColor: "transparent",
@@ -14,30 +15,11 @@ const tabs = {
 
 export default function ProjectsTabs() {
   return (
-   
-      <Tabs >
-        {" "}
-        <TabList>
-          <Tab>Web Development</Tab>
-          <Tab>Video Production</Tab>
-          <Tab>Digital Design</Tab>
-          <Tab>Motion Graphics</Tab>
-        </TabList>{" "}
-        <TabPanel>
-          <h2 style={{ textAlign: "left" }}>Web Development</h2>
-       
-        </TabPanel>
-        <TabPanel>
-          <h2>Video Production</h2>
-         
-        </TabPanel>
-        <TabPanel>
-          <h2>Digital Design</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Motion Graphics</h2>
-        </TabPanel>
-      </Tabs>
-
+   <div>
+    <AboutMe />
+    <AboutMe />
+    <AboutMe />
+    <AboutMe />
+    </div>                                                                                                                                             
   )
 }
